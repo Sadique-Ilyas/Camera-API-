704a7dfe0ec0573d19f7d6f7879e22077b87b9e1
Sadique Ilyas <sadiqueilyasfifa@gmail.com>
2020-07-19 11:43:18 +0530

    Initial Commit [Camera API]

---
+++ app/src/main/java/com/example/camera_api/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.camera_api;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.View;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class MainActivity extends AppCompatActivity {
+    String currentImagePath = null;
+    static final int IMAGE_REQUEST = 1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void captureImage(View view)
+    {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+        if (cameraIntent.resolveActivity(getPackageManager())!=null)
+        {
+            File imageFile = null;
+
+            try {
+                imageFile = getImageFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            if (imageFile!=null)
+            {
+                Uri imageUri = FileProvider.getUriForFile(this,"com.example.android.fileprovider1",imageFile);
+                cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT,imageUri);
+                startActivityForResult(cameraIntent,IMAGE_REQUEST);
+            }
+        }
+    }
+
+    public void displayImage(View view)
+    {
+        Intent intent = new Intent(this,DisplayImage.class);
+        intent.putExtra("image_path",currentImagePath);
+        startActivity(intent);
+    }
+
+    private File getImageFile() throws IOException {
+        String timeStamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
+        String imageName = "jpg_"+timeStamp+"_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        File imageFile = File.createTempFile(imageName,".jpg",storageDir);
+        currentImagePath = imageFile.getAbsolutePath();
+        return imageFile;
+    }
+}
